0df6eefd545d251593b6bd3ad53a423d178c5275
Eloi Gil <eloigil6@gmail.com>
2017-11-23 16:53:57 +0100

    yt working

---
+++ routes/search.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+const Playlist = require("../models/playlist").Playlist;
+
+router.post("/playlist/search/:", (req, res) => {
+  var db = req.DubJubeDB;
+  console.log(req.body);
+  db.playlists.find({
+    "name": req.body.name
+  }, (err, data) => {
+    if (err) { return err; }
+    console.log(data);
+  });
+});
+
+// router.post("/playlist/search", (req, res, next) => {
+//   const playlistId = req.params.playlistID;
+//   const query = req.body.searchQuery;
+//   axios
+//     .get(/**/)
+//     .then(response => {
+//       const data = {
+//         tempSongs: response.data.items,
+//         playlistId: playlistId
+//       };
+//       // console.log("data", data);
+//       res.render("playlist/addSong", data);
+//     })
+//     .catch(err => console.log(err));
+// });
